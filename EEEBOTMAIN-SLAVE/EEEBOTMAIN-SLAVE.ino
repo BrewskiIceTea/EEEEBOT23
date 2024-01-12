521512af9bb658ba7e62fca7bbabde1d10bb0b77
Brewskii1 <74499652+Brewskii1@users.noreply.github.com>
2024-01-12 05:53:54 +0000

    Update EEEBOTMAIN-SLAVE.ino

--- EEEBOTMAIN-SLAVE/EEEBOTMAIN-SLAVE.ino
+++ EEEBOTMAIN-SLAVE/EEEBOTMAIN-SLAVE.ino
@@ -1,5 +1,6 @@
 #include <ESP32Encoder.h>
-#include <stdio.h>  
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <Wire.h>
 
 #define enA 33  //EnableA command line - should be a PWM pin
@@ -42,61 +43,77 @@ float newPosition;
 int parked = 0;
 //I2C Message
 char message;
-void setup() {
-  // put your setup code here, to run once:
+char encoderBuffer[7];
 
-  pinMode(INa, OUTPUT);
-  pinMode(INb, OUTPUT);
-  pinMode(INc, OUTPUT);
-  pinMode(INd, OUTPUT);
-  // pinMode(enA, OUTPUT);
-  // pinMode(enB, OUTPUT);  //if defining some pins as PWM, DON'T set them as OUTPUT!
+//encodes 0-255 to 100-520 range that was found for the servo.
+void changeSteeringAngle(float angle){
+  steeringAngle = (angle * 1.65) + 100;
+  ledcWrite(servoChannel, steeringAngle);
+    // Serial.print("Steering Angle: ");
+    // Serial.println(abs(steeringAngle));
+}
 
-  // configure LED PWM functionalitites
-  ledcSetup(ledChannela, freq, resolution);
-  ledcSetup(ledChannelb, freq, resolution);
-  ledcSetup(servoChannel, servoFrequency, servoResolution); //servo setup on PWM2, 50Hz, 12-bit (0-4096)
+void wiggle(int wiggleno){
+  for (int i=0; i < wiggleno; i++){
+    changeSteeringAngle(0);
+    delay(100);
+    changeSteeringAngle(255);
+    delay(100);
+    Serial.println(i);
+    Serial.print("Wiggleno: ");
+    Serial.println(wiggleno);
+  }
+}
 
-  //attach the channel to the GPIO to be controlled
- 
-  ledcAttachPin(enA, ledChannela);
-  ledcAttachPin(enB, ledChannelb);
-  ledcAttachPin(servoPin, servoChannel);
-
-  //Setup I2C with other ESP32
-  Wire.begin(0x08);             // join i2c bus with address 8
-  Wire.onReceive(receiveEvent); // create a receive event
-  
-  //initialise Serial Communication
-  Serial.begin(9600);
-  Serial.println("ESP32 Running");  //sanity Check
-
-  encoder1.attachHalfQuad ( 36, 39 );
-  encoder1.setCount ( 0 );
-  encoder2.attachHalfQuad ( 34, 35 );
-  encoder2.setCount ( 0 );
-  goBackwards();
-  //setup the steering angle
-  straight();
-  
-  delay(3000);
-  motors(speedSetting, speedSetting);  //make a call to the 'motors' function and provide it with a value for each of the 2 motors - can be different for each motor - using same value here for expedience
-  Serial.print("Starting");
-
+void straight(){
+  changeSteeringAngle(126);
 }
 
 
-void loop() {
-  // wiggle(3);
-  //inputSteeringAngle();
-  edifferential(0);
+void checkEncoders(){
+  //ENCODER CODE
+  newPosition = ((encoder1.getCount() / 2)+(encoder2.getCount() / 2))/2 ;
+  // Serial.println(newPosition);
+}
 
-  edifferential(0.5);
-  // goForwards();
-  // eventAct();
-  // Serial.print("Motor speeds: ");
-  // Serial.println(speedSetting);
+void checkEncoder1(){
+  //ENCODER CODE
+  newPosition = (encoder1.getCount() / 2);
+  Serial.println(newPosition);
+}
+
+void checkEncoder2(){
+  //ENCODER CODE
+  newPosition = (encoder2.getCount() / 2);
+  Serial.println(newPosition);
+}
+
+
+
+// Remember that the checkEncoders is an average between the two, so distance can be different give a non centred eDifferential.
+void goDistance(float distance){ 
   checkEncoders();
+  float tempPosition = newPosition;
+  while (abs(newPosition - tempPosition) < distance){
+    checkEncoders();
+    Serial.print("\nTarget Position: ");
+    Serial.print(tempPosition);
+    Serial.print("\nCurrent Position: ");
+    Serial.print(newPosition);
+    delay(50);
+  }
+}
+
+void edifferential(float angle){ // Takes value between 0-1 and translates that to ratio of left motor speed to right motor speed.
+  float leftspeed, rightspeed;
+  if (angle < 0.5){
+    leftspeed = angle * speedSetting * 2;
+    rightspeed = speedSetting;
+  } else {
+    rightspeed = (1 - angle) * speedSetting * 2;
+    leftspeed = speedSetting; 
+  }
+  motors(leftspeed, rightspeed);
 }
 
 void motors(int leftSpeed, int rightSpeed) {
@@ -121,33 +138,11 @@ void goBackwards() {
   digitalWrite(INd, HIGH);
 }
 
-void goClockwise() {
-  digitalWrite(INa, HIGH);
-  digitalWrite(INb, HIGH);
-  digitalWrite(INc, LOW);
-  digitalWrite(INd, LOW);
-}
-
-void goAntiClockwise() {
-  digitalWrite(INa, LOW);
-  digitalWrite(INb, LOW);
-  digitalWrite(INc, HIGH);
-  digitalWrite(INd, HIGH);
-}
-
 void stopMotors() {
   digitalWrite(INa, LOW);
   digitalWrite(INb, LOW);
   digitalWrite(INc, LOW);
   digitalWrite(INd, LOW);
-}
-
-//encodes 0-255 to 100-520 range that was found for the servo.
-void changeSteeringAngle(float angle){
-  steeringAngle = (angle * 1.65) + 100;
-  ledcWrite(servoChannel, steeringAngle);
-    Serial.print("Steering Angle: ");
-    Serial.println(abs(steeringAngle));
 }
 
 // input steering angle manually through the serial monitor.
@@ -157,92 +152,120 @@ void inputSteeringAngle(){
     changeSteeringAngle(steeringAngle);
   }
 }
-//
+
+// events to be made upon different requests of the Master ESP32
 void eventAct(){
   switch(message)
   {
-    case 'S':
+    case 's':
+      break; // REMOVE FUNCTIONALITY
+      Serial.println("STOP RECIEVED");
       if (parked == 0){
       park();
-      }
-      else {
+      } else {
         stopMotors();
+        Serial.println("DONE");
         changeSteeringAngle(127);
         while (true){
-
         }
       }
       break;
+    case 'w':
+      Serial.print("WIGGLING");
+      delay(50);
+      wiggle(1);
+      delay(50);
+      message = ' ';
+      break;
+      
   }
-
 }
-void receiveEvent(int howMany){
+
+void receiveEvent(){
   Serial.println("\nMessage Recieved");
   message = ' ';
-  while (Wire.available()){  // loop whilst bus is busy
-    message = Wire.read();     // receive data byte by byte 
-  }
+  while (Wire.available()) {
+    char c = Wire.read();
+    message = message + c;
   Serial.println(message);    // write string to serial monitor
   delay(100);
-} 
-
-void checkEncoders(){
-  //ENCODER CODE
-  newPosition = ((encoder1.getCount() / 2)+(encoder2.getCount() / 2))/2 ;
-  Serial.println(newPosition);
+  } 
 }
 
-int wiggle(int wiggleno){
-  for (int i=0; i < wiggleno; i++){
-    changeSteeringAngle(0);
-    delay(100);
-    changeSteeringAngle(255);
-    delay(100);
-    Serial.println(i);
-    Serial.print("Wiggleno: ");
-    Serial.println(wiggleno);
-  }
-
-
-}
-void straight(){
-  changeSteeringAngle(126);
+void requestEvent() { // Writing Encoder values to the master ESP32.
+  Serial.println("request evented");
+  checkEncoders();
+  dtostrf(newPosition, 7, 2, encoderBuffer);
+  Wire.write(encoderBuffer);
 }
 
-void park(){
-  delay(400);
+// Series of functions to produce the parking feature.
+void park(){ 
+  Serial.print("parking");
   goForwards();
   goDistance(20);
   stopMotors();
   delay(200);
-  changeSteeringAngle(0);
-  edifferential(0);
+  changeSteeringAngle(255);
+  edifferential(0.8);
   goBackwards();
-  goDistance(100);
+  goDistance(40);
   edifferential(0.5);
-  changeSteeringAngle(127);
+  straight();
   message = 'g';
   parked = 1;
 }
 
-void goDistance(float distance){
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(INa, OUTPUT);
+  pinMode(INb, OUTPUT);
+  pinMode(INc, OUTPUT);
+  pinMode(INd, OUTPUT);
+  // pinMode(enA, OUTPUT);
+  // pinMode(enB, OUTPUT);  //if defining some pins as PWM, DON'T set them as OUTPUT!
+
+  // configure LED PWM functionalitites
+  ledcSetup(ledChannela, freq, resolution);
+  ledcSetup(ledChannelb, freq, resolution);
+  ledcSetup(servoChannel, servoFrequency, servoResolution); //servo setup on PWM2, 50Hz, 12-bit (0-4096)
+
+  //attach the channel to the GPIO to be controlled
+
+  ledcAttachPin(enA, ledChannela);
+  ledcAttachPin(enB, ledChannelb);
+  ledcAttachPin(servoPin, servoChannel);
+
+  //Setup I2C with other ESP32
+  Wire.begin(0x08);             // join i2c bus with address 8
+  Wire.onReceive(receiveEvent); // create a receive event
+  Wire.onRequest(requestEvent); // create a request event
+
+  //initialise Serial Communication
+  Serial.begin(9600);
+  Serial.println("ESP32 Running");  //sanity Check
+  encoder1.attachHalfQuad ( 36, 39 );
+  encoder1.setCount ( 0 );
+  encoder2.attachHalfQuad ( 34, 35 );
+  encoder2.setCount ( 0 );
+  goBackwards();
+  //setup the steering angle
+  straight();
+  delay(100);
+  wiggle(3);
+  delay(1500);
+  motors(speedSetting, speedSetting);  //make a call to the 'motors' function and provide it with a value for each of the 2 motors - can be different for each motor - using same value here for expedience
+  Serial.println("Starting...");
+  straight();
+}
+
+void loop() {
+  eventAct();
   checkEncoders();
-  float targetPosition = newPosition +distance;
-  while (newPosition < targetPosition){
-    checkEncoders();
-    // Serial.print("Target Position: ");
-    // Serial.print(targetPosition);
-    // Serial.print("Current Position: %f");
-    // Serial.print(newPosition);
-    float targetPosition = newPosition + distance;
-    delay(50);
-  }
+  delay(100);
 }
 
-void edifferential(float angle){ // takes value between 0-1 and translates that to ratio of left motor speed to right motor speed.
-  float leftspeed, rightspeed;
-  leftspeed = angle * speedSetting;
-  rightspeed = (1 - angle)*speedSetting ;
-  motors(leftspeed, rightspeed);
 
-}
+
+
+

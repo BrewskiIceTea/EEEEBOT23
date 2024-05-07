2958430892a22be228414e1633a4c9f6e8340e6e
BrewskiIceTea <74499652+BrewskiIceTea@users.noreply.github.com>
2024-05-07 12:27:59 +0100

    Update main.hpp
    Adding PID to hpp

--- OpenCV-Template/main.hpp
+++ OpenCV-Template/main.hpp
@@ -4,5 +4,5 @@
 // Function declarations
 void setup(void);                   //Function to configure the GPIO and devices for operation
 int main(int argc, char** argv);
-
+int PID(double setpoint, double pv);
 #endif // MAIN_HPP_INCLUDED
